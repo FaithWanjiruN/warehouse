08962f37814c5f265d47fb829d686e248b681e16
Esther Wangari <esskaree@gmail.com>
2024-03-17 01:04:35 +0300

    commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register(Movie),
-admin.site.register(Shows),
-admin.site.register(Bookings),
--- myapp/models.py
+++ myapp/models.py
@@ -3,21 +3,10 @@ from django.contrib.auth.models import User
 
 
 # Create your models here
-class Movie(models.Model):
-    movie=models.AutoField(primary_key=True)
-    movie_name=models.CharField(max_length=50)
-    movie_des=models.TextField()
-    movie_rating=models.DecimalField(max_digits=3, decimal_places=1)
-    movie_poster=models.ImageField(upload_to='movies/poster', default="movies/poster/not.jpg")
-    movie_genre=models.CharField(max_length=50,default="Action | Comedy | Romance")
-    movie_duration=models.CharField(max_length=10, default="2hr 45min")
-
-    def __str__(self):
-        return self.movie_name
 
 class Shows(models.Model):
     shows=models.AutoField(primary_key=True)
-    cinema=models.ForeignKey('Cinema',on_delete=models.CASCADE, related_name='cinema_show')
+
     movie=models.ForeignKey('Movie',on_delete=models.CASCADE, related_name='movie_show')
     time=models.CharField(max_length=100)
     date=models.CharField(max_length=15, default="")
